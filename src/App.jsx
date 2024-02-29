b13765e5e9cec7704e0fa061e1a704ca0376290b
Harsh <harshb15003@gmail.com>
2024-02-29 18:25:40 +0530

    design

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,19 @@
-/*import styles from './Style'
+ import React from "react";
 
-import React from 'react'
-
-import {
-  Navbar,
-  Billing,
-  CardDeal,
-  Business,
-  Clients,
-  CTA,
-  Stats,
-  Footer,
-  Testiomonals,
-  Hero,
-} from "./components"
-
+import Home from "./Pages/Home";
+import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
 
 const App = () => (
-
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-       < Navbar/>
-      </div>
-    </div>
-
-
-
-
-   
-    <div className={`bg-primary ${styles.flexStart}`}>
-
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-
-
-
-
-    <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Billing />
-        <CardDeal />
-        <Business />
-        <Clients />
-        <CTA />
-        <Stats />
-        <Footer />
-        <Testiomonals />
-      </div>
-    </div>
-
-
-  </div>
-)
-
-
-export default App */
-
-
-
-
-
-import React from "react";
-
-import styles from "./style";
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
-
-const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
-    </div>
-
-    <div className={`bg-black ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-    
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
-  </div>
+ <div>
+<Router>
+<Routes>
+  <Route path="/" element=
+  {<Home/>}
+  />
+  {/* <Route path="/payment" element={<Payment/>}/> */}
+</Routes>
+</Router>
+ </div>
 );
 
 export default App;
