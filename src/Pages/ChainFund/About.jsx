13e194e3ccbd89c1a7abbd4a5304d0019e7fedf6
Akshat Gupta <akshatgtc@gmail.com>
2024-03-02 02:13:02 +0530

    done

--- src/Pages/ChainFund/About.jsx
+++ src/Pages/ChainFund/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import "/Users/harsh/Desktop/practicum/ChainFund/src/index.css";
+import "../../../src/index.css";
 
 const About = () => {
   return (
