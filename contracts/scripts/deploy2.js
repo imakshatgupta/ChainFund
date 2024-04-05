e7f4f325a66c01f8c1b0332a4659fbdb37cfa6b2
Harsh <harshb15003@gmail.com>
2024-04-05 16:37:53 +0530

    changes

---
+++ contracts/scripts/deploy2.js
@@ -0,0 +1,23 @@
+const hre = require('hardhat');
+
+const main = async () => {
+    const campaignFactory = await hre.ethers.getContractFactory("CrowdFunding");
+    const campaignContract = await campaignFactory.deploy();
+  
+    await campaignContract.deployed();
+  
+    console.log("campaign address: ", campaignContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
+
